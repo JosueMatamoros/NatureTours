619a0e5a605fd90255f2590893ad56a65a108099
JosueMatamoros <1002matamoros@gmail.com>
2026-01-08 13:53:46 -0600

    fix: paypal max hight animation

--- src/components/ui/Collapse.jsx
+++ src/components/ui/Collapse.jsx
@@ -18,7 +18,7 @@ export default function Collapse({ show, duration = 300, children, className = "
 
   const base =
     "overflow-hidden transition-all ease-out will-change-[max-height,opacity,transform]";
-  const open = "max-h-[700px] opacity-100 translate-y-0 scale-100 ";
+  const open = "max-h-full  opacity-100 translate-y-0 scale-100 ";
   const closed = "max-h-0 opacity-0 -translate-y-1 scale-[0.99]";
 
   return (
